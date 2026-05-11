432230175c924d6738139c3420404b762bb171cf
strawberryshorcake <cunha.esthefany@escola.pr.gov.br>
2026-05-11 10:28:14 -0300

    salvar

--- main.js
+++ main.js
@@ -13,3 +13,8 @@ for(let i=0;i <botoes.length; i++){
        textos[i].classList.add("ativo");
     }
 }
+
+
+const contadores = document.querySelectorAll(".contador");
+
+contadores[0].textContent = "Contagem regressiva"
