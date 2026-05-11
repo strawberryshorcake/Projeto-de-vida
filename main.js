e247613881c59af717f602d2894e96f8523c44ec
strawberryshorcake <cunha.esthefany@escola.pr.gov.br>
2026-05-11 10:12:03 -0300

    salvar

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ for(let i=0;i <botoes.length; i++){
 
        for(let j=0;j<botoes.length;j++){
            botoes[j].classList.remote("ativo");
+           textos[j].classList.remote("ativo");
        }
 
        botoes[i].classList.add("ativo");
