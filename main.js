df17146ae39fac3f1cef891ff3a03cf27d6d6e1a
strawberryshorcake <cunha.esthefany@escola.pr.gov.br>
2026-05-18 08:04:18 -0300

    salvar

--- main.js
+++ main.js
@@ -20,3 +20,6 @@ const tempoObjetivo1 = new Date("2026-10-0500:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = calculaTempo(tempoObjetivo);
+
+function calculaTempo(tempoObjetivo){
+}
